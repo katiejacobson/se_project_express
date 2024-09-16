c12b13e384d9fc8c550e688c37926cde44a59857
Katie Jacobson <katieabole@gmail.com>
2024-09-15 20:45:36 -0400

    fix lint errors

--- models/clothingItems.js
+++ models/clothingItems.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-let validator = require("validator");
+const validator = require("validator");
 
 const clothingItemSchema = new mongoose.Schema({
   name: {
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-let validator = require("validator");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   name: {
